99ef2a62d3dcfabe68ccacc991ae07963d560033
hugomarqs <hugomaarqz@gmail.com>
2021-04-23 22:29:09 +0100

    v2

--- app/src/main/java/com/example/culturetraveler/DisplayInicial.java
+++ app/src/main/java/com/example/culturetraveler/DisplayInicial.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DisplayInicial extends AppCompatActivity {
